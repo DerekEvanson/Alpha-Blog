224e47354fd928f4e84a3f95400367b12dce1173
Derek Evanson <evanson.derek@gmail.com>
2020-04-28 15:08:47 -0500

    /articles/new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,18 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title))
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            #redirect_to article_path(@article)
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
 end
