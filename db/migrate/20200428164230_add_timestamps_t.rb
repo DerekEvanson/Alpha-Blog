7f9ea368de39cffc42fdc2206e9d5a84ae9c3a37
Derek Evanson <evanson.derek@gmail.com>
2020-04-28 11:48:31 -0500

    Articles Schema

---
+++ db/migrate/20200428164230_add_timestamps_t.rb
@@ -0,0 +1,6 @@
+class AddTimestampsT < ActiveRecord::Migration[6.0]
+  def change
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :update_at, :datetime
+  end
+end
---
+++ db/migrate/20200428164550_add_timestamps_to_articles.rb
@@ -0,0 +1,7 @@
+class AddTimestampsToArticles < ActiveRecord::Migration[6.0]
+  def change
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :update_at, :datetime
+  end
+end
+
