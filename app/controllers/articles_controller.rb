cec5d336eaba1a2b7306aa560f5b2867263bae51
Derek Evanson <evanson.derek@gmail.com>
2020-05-01 02:47:41 -0500

    Refactor and Errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,51 +1,52 @@
 class ArticlesController < ApplicationController
-    before_action :set_article, only: [:show, :edit, :update, :destroy]
-    
-    def show 
-    end
+before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-    def index
-        @articles = Article.all
-    end
+def show
+end
 
-    def new
-        @article = Article.new
-    end
+def index
+@articles = Article.all
+end
 
-    def edit
-    end
+def new
+@article = Article.new
+end
 
-    def create
-        @article = Article.new(article_params)
-        if @article.save
-            flash[:notice] = "Article was created successfully."
-            redirect_to @article #redirect_to article_path(@article)
-        else
-            render 'new'
-        end
-    end
+def edit
+end
 
-    def update
-        if @article.update(article_params)
-            flash[:notice] = "Article was updated successfully."
-            redirect_to @article 
-        else
-            render 'edit'
-        end
-    end 
+def create
+	@article = Article.new(article_params)
+	if @article.save
+		flash[:notice] = "Article was created successfully."
+		redirect_to @article
+	else
+		render 'new'
+	end
+end
 
-    def destroy
-        @article.destroy
-        redirect_to articles_path
-    end
+def update
+	if @article.update(article_params)
+		flash[:notice] = "Article was updated successfully."
+		redirect_to @article
+	else
+		render 'edit'
+	end
+end
 
-    private
+def destroy
+	@article.destroy
+	redirect_to articles_path
+end
 
-    def set_article
-        @article = Article.find(params[:id])
-    end
+private
 
-    def article_params
-        params.require(:article).permit(:title)
-    end
+def set_article
+  @article = Article.find(params[:id])
+end
+
+def article_params
+  params.require(:article).permit(:title, :description)
+end
+
 end
