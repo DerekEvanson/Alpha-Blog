6d75666c2206c0f4b43447df580c77285135f195
Derek Evanson <evanson.derek@gmail.com>
2020-05-01 11:38:39 -0500

    Routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
- root 'pages#home'
- get 'about', to: 'pages#about'
- resources :articles
- get 'signup', to: 'users#new'
- post 'users', to: 'users#create'
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
