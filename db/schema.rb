688fd2e1596a239b6ccc56966f75216be2f9b8b0
Derek Evanson <evanson.derek@gmail.com>
2020-05-03 19:50:09 -0500

    Admin Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_170441) do
+ActiveRecord::Schema.define(version: 2020_05_04_000905) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_04_30_170441) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
